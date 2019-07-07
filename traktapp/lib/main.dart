6949076b6e65b6fb80bcd17465c07386445b77f1
cgrbyk <cgrbyk@gmail.com>
2019-07-07 14:49:27 +0300

    movies çekildi

--- traktapp/lib/main.dart
+++ traktapp/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart'as http;
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
-
 
 void main() => runApp(MaterialApp(
       home: MyApp(),
@@ -15,25 +14,30 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    movies();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF2d3447),
       body: Container(),
-      
-      );
-        
-        
-      
-    
+    );
   }
 }
 
 movies() async {
-    final response = await http.get(
-        "https://api.trakt.tv/movies/popular?Content-Type=application/json&trakt-api-version=2&trakt-api-key=[da986893dbeb57da52760478c82af518b31d62425053c565845acc550f6430bf]");
-    var json = jsonDecode(response.body);
-    return json;
-  }
+  final response =
+      await http.get("https://api.trakt.tv/movies/popular", headers: {
+    'Content-Type': 'application/json',
+    'trakt-api-version': '2',
+    'trakt-api-key':
+        'da986893dbeb57da52760478c82af518b31d62425053c565845acc550f6430bf'
+  });
+  var json = jsonDecode(response.body);
+  return json;
+}
